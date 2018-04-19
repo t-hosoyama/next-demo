74bf801d8857882c5ad8fdf382f3db366d7ae898
[mediba]Terunobu Hosoyama <hosoyama@mediba.jp>
2018-04-19 10:04:00 +0900

    ポート指定できるよう修正

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const next = require('next')
+const argv = require('argv')
 
+argv.option({
+    name: 'port',
+    short: 'p',
+    type: 'integer',
+    description: 'Listen server port. default:3000',
+})
+
+const args = argv.run()
+const port = args.options.port || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -13,11 +23,11 @@ app
           return handle(req, res)
       })
 
-      server.listen(3000, (err) => {
+      server.listen(port, (err) => {
           if (err) {
             throw err
           }
-          console.log('> Ready on http://localhost:3000')
+          console.log(`> Ready on http://localhost:${port}`)
       })
 })
 .catch((ex) => {
