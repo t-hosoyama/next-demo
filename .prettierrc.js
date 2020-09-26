13dc4870e954abdb78dccf30e9fcf9cfdeecada9
hosoyama <hosoyama@gmail.com>
2020-09-27 03:57:34 +0900

    fix: code format

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "bracketSpacing": true,
+  "printWidth": 120,
+  "semi": false,
+  "singleQuote": false,
+  "tabWidth": 2,
+  "trailingComma": "none",
+  "useTabs": false
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,33 @@ import styled from "styled-components"
 import Layout from "~/components/Layout"
 import { color, url } from "~/utils/style"
 
-const IndexPage: NextPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <Title>Hello Next.js 👋</Title>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage: NextPage = () => {
+  return (
+    <$Container>
+      <Layout title="Home | Next.js + TypeScript Example">
+        <$Title>Hello Next.js 👋</$Title>
+        <$Body>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </$Body>
+      </Layout>
+    </$Container>
+  )
+}
 export default IndexPage
 
-const Title = styled.h1`
+const $Container = styled.div`
+  background-color: ${color.white};
+`
+
+const $Title = styled.h1`
   color: ${color.inkBlack};
   &::before {
     content: ${url("/assets/images/nico.gif")};
   }
+`
+
+const $Body = styled.p`
+  color: ${color.inkBlack};
 `
